a71d089ab0b21e64b8c47712c308ff4ea5212f99
xqgdtx1 <xqgdtx1@gmail.com>
2021-04-12 11:17:47 +0800

    加入test-code模块方便测试源码中未使用代码

---
+++ zlt-demo/test-code/src/main/java/TestUtils.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+import org.apache.commons.lang3.StringUtils;
+
+public class TestUtils extends TestCase
+{
+    public void testArrayToCommaDelimitedString(){
+        String[] arr = new String[]{"1", "2", "3"};
+    }
+
+}
