b32995e3607289f2cf9bb2b8f540fc84ee3dcada
“xqgdtx1@sina.com” <xqgdtx1@sina.com>
2021-04-13 18:37:33 +0800

    加入代码测试片段

--- zlt-demo/test-code/src/main/java/TestUtils.java
+++ zlt-demo/test-code/src/main/java/TestUtils.java
@@ -1,12 +1,13 @@
 
 import junit.framework.TestCase;
 
+import org.apache.catalina.User;
 import org.junit.Test;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
-import java.util.Arrays;
+import java.util.*;
 
 
 public class TestUtils extends TestCase
@@ -22,4 +23,32 @@ public class TestUtils extends TestCase
         Arrays.asList(strings).forEach(System.out::println);
     }
 
+    @Test(expected = NoSuchElementException.class)
+    public void testOptional() {
+        Optional<User> emptyOpt = Optional.empty();
+        emptyOpt.get();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNPE() {
+        LinkedList<String> strings = new LinkedList<>();
+//        Optional<User> opt = Optional.of(user);
+        LinkedList<String> s = Optional.ofNullable(strings).orElseGet(()->{
+            LinkedList<String> strings1 = new LinkedList<>();
+            strings1.add("abc");
+            return strings1;
+        });
+    }
+    @Test
+    public void testStringNull() {
+        String name = "John";
+        Optional<String> opt = Optional.ofNullable(name);
+        String s = opt.get();
+        boolean present = opt.isPresent();
+        opt.ifPresent(h -> {
+            assertEquals(name,s);
+            System.out.println(h);});
+
+        assertEquals("John", s);
+    }
 }
